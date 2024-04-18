5cdb3af1f9138cb9b945587fdae2ebb34df55f25
Robert Alexander <3751418+ralexander-phi@users.noreply.github.com>
2024-04-18 21:53:23 +0400

    Fix build

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ cp .domains public/
 git branch -D pages || echo "doesn't exist"
 git switch --orphan pages
 git add public/
+git add public/.domains
 git mv ./public/* ./
+git mv ./public/.domains ./
 git commit -m "Update pages"
 git push --set-upstream origin pages --force
