0756cb917672c63e16ea46afe4d1da91f7638cfb
Robert Alexander <3751418+ralexander-phi@users.noreply.github.com>
2024-04-18 21:32:12 +0400

    Fix build

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ set -e
 git checkout main
 rm -Rf public/
 hugo --gc --minify --baseURL `head -n 1 .domains`
+cp .domains public/
 git branch -D pages || echo "doesn't exist"
 git switch --orphan pages
-git add .domains
 git add public/
 git mv ./public/* ./
 git commit -m "Update pages"
