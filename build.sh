191bd7fe5b33b9ebc66a5ec7238cf3ddb3ae73a6
Robert Alexander <3751418+ralexander-phi@users.noreply.github.com>
2024-04-18 19:33:26 +0400

    Custom domain

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 git checkout main
 rm -Rf public/
-hugo --gc --minify --baseURL /alwaysopen/
+hugo --gc --minify --baseURL `head -n 1 .domains`
 git branch -D pages || echo "doesn't exist"
 git switch --orphan pages
 git add public/
